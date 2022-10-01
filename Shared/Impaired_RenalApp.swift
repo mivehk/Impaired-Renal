a9e72b727e28627d4fa6083bbd43c27c259a1690
mivehk <k.mivehnejad@gmail.com>
2022-10-01 15:02:47 -0400

    Initial Commit

---
+++ Shared/Impaired_RenalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Impaired_RenalApp.swift
+//  Shared
+//
+//  Created by kayvan mivehnejad on 10/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Impaired_RenalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
